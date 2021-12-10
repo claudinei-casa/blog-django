ef2c60897e2d52b6c6ad55b6becdc85f99d84433
claudinei <claudinei.casa@ufpr.br>
2021-12-10 10:48:50 -0300

    adds admin config cars

---
+++ cars/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Cars
+
+
+@admin.register(Cars)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("make", "model", "slug", "year", "color",
+                    "price", "mileage", "description")
+    list_filter = ("model", "color", "make", "slug")
+    search_fields = ("make", "model", "slug")
+    prepopulated_fields = {"slug": ("make", "model",)}
+    ordering = ["make", "year"]
