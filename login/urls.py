10d68d4ef6c37b8ed0a197cc3c314a8e0faf5512
claudinei <claudinei.casa@ufpr.br>
2021-12-16 11:33:53 -0300

    adds login and template for login

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import login
+from .views import login_user, submit_user, logout_user
 
 app_name = 'login'
 urlpatterns = [
-    path('', login, name='login'),
+    path('', login_user, name='login'),
+    # submit sem barra no final pois é um post. se fosse get tem que ter
+    path("submit", submit_user, name='submit'),
+    path("logout/", logout_user, name="logout_user"),
 ]
--- login/views.py
+++ login/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
 
 
-def login(request):
+def login_user(request):
     return render(request, 'login.html')
+
+
+def submit_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('user')
+        password = request.POST.get('password')
+        usr = authenticate(username=username, password=password)
+        if usr is not None:
+            login(request, usr)
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect('/login')
+    else:
+        return HttpResponseRedirect('/login')
+
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect('/')
