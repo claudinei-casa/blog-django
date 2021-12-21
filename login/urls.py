483f58f1b461861ab51b27a74e8fda823ac4b4b7
claudinei <claudinei.casa@ufpr.br>
2021-12-20 22:59:15 -0300

    Modify login views and routes adds create_user

--- login/urls.py
+++ login/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import login_user, submit_user, logout_user
+
+from .views import login_user, submit_user, logout_user, create_user
 
 app_name = 'login'
 urlpatterns = [
@@ -7,4 +8,5 @@ urlpatterns = [
     # submit sem barra no final pois é um post. se fosse get tem que ter
     path("submit", submit_user, name='submit'),
     path("logout/", logout_user, name="logout_user"),
+    path("create_user/", create_user, name="create_user"),
 ]
--- login/views.py
+++ login/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .create_user import CreateUser
 
 
 def login_user(request):
@@ -16,11 +18,22 @@ def submit_user(request):
             login(request, usr)
             return HttpResponseRedirect('/')
         else:
-            return HttpResponseRedirect('/login')
-    else:
-        return HttpResponseRedirect('/login')
+            messages.error(request, 'Invalid username or password')
+    return HttpResponseRedirect('/login')
 
 
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect('/')
+
+
+def create_user(request):
+    if request.method == 'POST':
+        form_create_user = CreateUser(request.POST)
+        if form_create_user.is_valid():
+            form_create_user.save()
+            messages.success(request, 'User created successfully')
+            return render(request, 'create_user.html', {'form_create_user': form_create_user})
+        else:
+            messages.error(request, 'Error creating user')
+    return render(request, 'create_user.html', context={'form_create_user': CreateUser})
