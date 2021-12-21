7aaa5fe1411bf73b8b46debe15b5df4e8d86c230
claudinei <claudinei.casa@ufpr.br>
2021-12-20 22:58:01 -0300

    Modify Blog, admin and routes.

--- blog/admin.py
+++ blog/admin.py
@@ -7,8 +7,10 @@ from .models import Post
 # to review
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("title", "slug", "author", "created_on", "last_modified")
+    list_display = ("title", "author", "created_on", "last_modified", "slug",)
+    readonly_fields = ("created_on", "last_modified",)
     list_filter = ("created_on", "author", "title")
     search_fields = ("title", "body", "slug")
     prepopulated_fields = {"slug": ("title",)}
     ordering = ["last_modified", "created_on"]
+    date_hierarchy = "created_on"
